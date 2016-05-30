cdc9e99be8f901b6a187bb7ae659d1ec5b45c1f9
Niranjan Rajendran <niranjan94@yahoo.com>
2016-05-30 18:27:29 +0530

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+if [ "$TRAVIS_BRANCH" != "master" ]
+then
+  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
+  exit 0
+fi
+
+rev=$(git rev-parse --short HEAD)
+
+git config --global user.name "Niranjan Rajendran"
+git config --global user.email "niranjan94@yahoo.com"
+
+git clone "https://$GH_TOKEN@github.com/niranjan94/spa-deploy-demo.git" gh-pages
+cd gh-pages
+git checkout gh-pages
+yes | cp -rf  ../dist/* ./*
+git add .
+git commit -m "[${rev}] Updated"
+git push -q origin gh-pages
---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <hello></hello>
+    <p>
+      Welcome to the SPA deploy demo!
+    </p>
+    <p>
+      To get a better understanding of how this works, check out
+      <a href="https://github.com/niranjan94/spa-deploy-demo/blob/master/deploy.sh" target="_blank">deploy.sh</a>.
+    </p>
+    <p>
+      You may also want to checkout
+      <a href="https://github.com/niranjan94/spa-deploy-demo/blob/master/.travis.yml" target="_blank">.travis.yml</a>.
+    </p>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+#app {
+  color: #2c3e50;
+  margin-top: -100px;
+  max-width: 600px;
+  font-family: Source Sans Pro, Helvetica, sans-serif;
+  text-align: center;
+}
+
+#app a {
+  color: #42b983;
+  text-decoration: none;
+}
+
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
